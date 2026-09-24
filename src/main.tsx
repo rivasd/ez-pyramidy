ae5cc0ce3c8c9922619828d1db56875a9a95e952
rivasd <rivasdaniel1992@gmail.com>
2026-09-24 10:14:54 -0400

    fix: styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'jspsych/css/jspsych.css'
+// import 'jspsych/css/jspsych.css'
 import "@mantine/core/styles.css"
 import './index.css'
 import Root from './Root.tsx'
